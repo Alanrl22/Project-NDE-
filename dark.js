a6805857f1e83e7ff610a0484eb2dba59f24707d
ahamel1 <ahamel.wcs44@gmail.com>
2020-03-19 16:12:20 +0100

    ajout lightmode js

---
+++ dark.js
@@ -0,0 +1,11 @@
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+const toggler = document.querySelector('#toggler');
+toggler.addEventListener('change', switchTheme, false);
